6a208f115fd271f2e5e473fce2b499fc936fa0d8
xtian7489 <xtian7489@gmail.com>
2025-09-23 10:36:06 -0300

    feat: Eliminar la apertura en nueva pestaña al hacer clic en el tema

--- ext/lib/site/home-catalogo/topic-card/component.js
+++ ext/lib/site/home-catalogo/topic-card/component.js
@@ -32,7 +32,7 @@ export class TopicCard extends Component {
     const isProyectistaButton = findAncestor(e.target, 'proyectista-wrapper')
     const isVotarButton = findAncestor(e.target, 'votar-button-wrapper')
     if (!isSeguirButton && !isProyectistaButton && !isVotarButton)
-      window.open(`${window.location.pathname}/topic/${this.props.topic.id}`, '_blank');
+      window.open(`${window.location.pathname}/topic/${this.props.topic.id}`);
   }
   render() {
     const { topic, onVote, onProyectista, user, forum } = this.props
